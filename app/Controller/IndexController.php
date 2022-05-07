e929b9a4891ef5bb8b619198e100fe619a60bd0a
wujingfeng <1163377596@qq.com>
2022-05-07 15:31:57 +0800

    init

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+use App\Constants\BaseConstants;
+
+class IndexController extends AbstractController
+{
+    public function index()
+    {
+        $user = $this->request->input('user', 'Hyperf');
+        $method = $this->request->getMethod();
+
+        return [
+            'method' => $method,
+            "code"  => BaseConstants::NOT_FOUND,
+            'message' => "Hello {$user}.",
+        ];
+    }
+}
